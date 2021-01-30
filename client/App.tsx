81575b592f1880704074199ef2ef7837bc1b49f5
casneil <casneil.simpson@gmail.com>
2021-01-30 19:24:43 +0100

    adding apollo client

--- client/App.tsx
+++ client/App.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 // 3rd Party
 import {UtilityThemeProvider, Box, Text} from 'react-native-design-utility';
 import {theme} from './src/constants/theme';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
+
 const App = () => {
   return (
-    <UtilityThemeProvider theme={theme}>
-      <StatusBar barStyle="light-content" />
-      <Box bg="purpleLightest" f={1}>
-        <SafeAreaView>
-          <Text size="xl" center>
-            Hello
-          </Text>
-        </SafeAreaView>
-      </Box>
-    </UtilityThemeProvider>
+    <ApolloProvider client={client}>
+      <UtilityThemeProvider theme={theme}>
+        <StatusBar barStyle="light-content" />
+        <Box bg="purpleLightest" f={1}>
+          <SafeAreaView>
+            <Text size="xl" center>
+              Hello Casneil
+            </Text>
+          </SafeAreaView>
+        </Box>
+      </UtilityThemeProvider>
+    </ApolloProvider>
   );
 };
 
