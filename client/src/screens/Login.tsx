07cd863b5d09bc1eb1dac4e0b9228c486fbf085b
casneil <casneil.simpson@gmail.com>
2021-01-31 20:34:54 +0100

    added keyboard avoiding view

--- client/src/screens/Login.tsx
+++ client/src/screens/Login.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, {useEffect} from "react";
 // Native imports
-import {TextInput, TouchableOpacity, Image} from "react-native";
+import {
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Keyboard,
+  KeyboardAvoidingView,
+} from "react-native";
+
+import {TouchableWithoutFeedback} from "react-native-gesture-handler";
 import {useNavigation} from "@react-navigation/native";
 
 // Third Party
@@ -14,6 +22,7 @@ import {Box, Text} from "react-native-design-utility";
 import {useAuthContext} from "../components/context/AuthContext";
 // Emuns and Interfaces
 import {ILoginInterface} from "../components/Interfaces";
+import {ImageHeightEnum, StorageKeyEnum} from "../components/enums";
 //Resource
 const icon = require("../rsc/icon.jpg");
 // Styles
@@ -48,138 +57,149 @@ const Login = () => {
   });
 
   return (
-    <>
-      <Box my="sm">
-        <Text bold size="xl" center>
-          Login
-        </Text>
-      </Box>
-      <Box center my="sm">
-        <Box bg="white" style={{elevation: 20}} mb="lg" radius={100} w={110}>
-          <Image
-            source={icon}
-            style={{
-              overflow: "hidden",
-              height: 110,
-              width: 110,
-              borderRadius: 100,
-            }}
-          />
-        </Box>
-      </Box>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={async (values, {setSubmitting}) => {
-          setSubmitting(true);
-          const response = await login({
-            variables: values,
-          });
-          //@ts-ignore
-          setUserToken(response.data.login.token);
+    <KeyboardAvoidingView style={{flex: 1}} behavior="position">
+      <>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <Box my="sm">
+            <Text bold size="xl" center>
+              Login
+            </Text>
+          </Box>
+          <Box center my="sm">
+            <Box
+              bg="white"
+              style={{elevation: 20}}
+              mb="lg"
+              radius={100}
+              w={110}>
+              <Image
+                source={icon}
+                style={{
+                  overflow: "hidden",
+                  height: 110,
+                  width: 110,
+                  borderRadius: 100,
+                }}
+              />
+            </Box>
+          </Box>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={async (values, {setSubmitting}) => {
+              setSubmitting(true);
+              const response = await login({
+                variables: values,
+              });
+              //@ts-ignore
+              setUserToken(response.data.login.token);
 
-          setSubmitting(false);
-        }}>
-        {({handleChange, handleBlur, handleSubmit, values, errors}) => (
-          <>
-            <Box mx={60}>
-              <Box center>
-                <Box position="absolute" right={0}>
-                  <AntDesign name="mail" size={18} color="black" />
-                </Box>
-                <Box dir="row" align="center">
-                  <TextInput
-                    onChangeText={handleChange("email")}
-                    placeholder="Enter your email address"
-                    onBlur={handleBlur("email")}
-                    keyboardType={"email-address"}
-                    value={values.email}
-                    style={{
-                      borderBottomWidth: 1,
-                      width: "100%",
-                      borderBottomColor: "gray",
-                    }}
-                  />
-                </Box>
-                {errors.email && (
-                  <Text style={{fontSize: 10, color: "red"}}>
-                    {errors.email}
-                  </Text>
-                )}
-              </Box>
-              <Box>
-                <Box dir="row" align="center">
-                  <Box position="absolute" right={0}>
-                    <AntDesign name="lock" size={18} color="black" />
+              setSubmitting(false);
+            }}>
+            {({handleChange, handleBlur, handleSubmit, values, errors}) => (
+              <>
+                <Box mx={60}>
+                  <Box>
+                    <Box dir="row" align="center">
+                      <Box position="absolute" right={0}>
+                        <AntDesign name="mail" size={18} color="black" />
+                      </Box>
+                      <Box dir="row" align="center">
+                        <TextInput
+                          onChangeText={handleChange("email")}
+                          placeholder="Enter your email address"
+                          onBlur={handleBlur("email")}
+                          keyboardType={"email-address"}
+                          value={values.email}
+                          style={{
+                            borderBottomWidth: 1,
+                            width: "100%",
+                            borderBottomColor: "gray",
+                          }}
+                        />
+                      </Box>
+                    </Box>
+                    {errors.email && (
+                      <Text style={{fontSize: 10, color: "red"}}>
+                        {errors.email}
+                      </Text>
+                    )}
                   </Box>
-                  <TextInput
-                    onChangeText={handleChange("password")}
-                    onBlur={handleBlur("password")}
-                    placeholder="Password"
-                    placeholderTextColor="gray"
-                    secureTextEntry={true}
-                    value={values.password}
-                    style={{
-                      borderBottomWidth: 1,
-                      width: "100%",
-                      borderBottomColor: "gray",
-                    }}
-                  />
+                  <Box>
+                    <Box dir="row" align="center">
+                      <Box position="absolute" right={0}>
+                        <AntDesign name="lock" size={18} color="black" />
+                      </Box>
+                      <TextInput
+                        onChangeText={handleChange("password")}
+                        onBlur={handleBlur("password")}
+                        placeholder="Password"
+                        placeholderTextColor="gray"
+                        secureTextEntry={true}
+                        value={values.password}
+                        style={{
+                          borderBottomWidth: 1,
+                          width: "100%",
+                          borderBottomColor: "gray",
+                        }}
+                      />
+                    </Box>
+                    {errors.password && (
+                      <Text style={{fontSize: 10, color: "red"}}>
+                        {errors.password}
+                      </Text>
+                    )}
+                  </Box>
                 </Box>
-                {errors.password && (
-                  <Text style={{fontSize: 10, color: "red"}}>
-                    {errors.password}
-                  </Text>
-                )}
-              </Box>
-            </Box>
-            <Box center my="sm">
-              <Box
-                bg="white"
-                w={100}
-                center
-                radius="lg"
-                style={{elevation: 10}}>
-                <TouchableOpacity onPress={handleSubmit}>
-                  <Text p={8} bold>
-                    Log in
-                  </Text>
-                </TouchableOpacity>
-              </Box>
-            </Box>
-            {/*Register Links*/}
-            <Box dir="row" justify="center" my={4}>
-              <Text size="sm">Don't have an account?</Text>
-              <TouchableOpacity
-                style={{marginLeft: 10}}
-                onPress={() => navigation.navigate("Register")}>
-                <Text
-                  size="sm"
-                  bold
-                  style={{
-                    textDecorationLine: "underline",
-                  }}>
-                  Register
-                </Text>
-              </TouchableOpacity>
-            </Box>
-            <Box dir="row" justify="center">
-              <Text size="sm">Forget your password?</Text>
-              <TouchableOpacity style={{marginLeft: 10}}>
-                <Text
-                  size="sm"
-                  bold
-                  style={{
-                    textDecorationLine: "underline",
-                  }}>
-                  Reset
-                </Text>
-              </TouchableOpacity>
-            </Box>
-          </>
-        )}
-      </Formik>
-    </>
+                <Box center my="sm">
+                  <Box
+                    bg="white"
+                    w={100}
+                    center
+                    radius="lg"
+                    style={{elevation: 10}}>
+                    <TouchableOpacity onPress={handleSubmit}>
+                      <Text p={8} bold>
+                        Log in
+                      </Text>
+                    </TouchableOpacity>
+                  </Box>
+                </Box>
+                {/*Register Links*/}
+                <Box dir="row" justify="center" my={4}>
+                  <Text size="sm">Don't have an account?</Text>
+                  <TouchableOpacity
+                    style={{marginLeft: 10}}
+                    onPress={() => navigation.navigate("Register")}>
+                    <Text
+                      size="sm"
+                      bold
+                      style={{
+                        textDecorationLine: "underline",
+                      }}>
+                      Register
+                    </Text>
+                  </TouchableOpacity>
+                </Box>
+                <Box dir="row" justify="center">
+                  <Text size="sm">Forget your password?</Text>
+                  <TouchableOpacity style={{marginLeft: 10}}>
+                    <Text
+                      size="sm"
+                      bold
+                      style={{
+                        textDecorationLine: "underline",
+                      }}>
+                      Reset
+                    </Text>
+                  </TouchableOpacity>
+                </Box>
+              </>
+            )}
+          </Formik>
+        </TouchableWithoutFeedback>
+      </>
+    </KeyboardAvoidingView>
   );
 };
 
--- client/src/screens/Register.tsx
+++ client/src/screens/Register.tsx
@@ -1,6 +1,13 @@
 import React from "react";
-import {TextInput, TouchableOpacity, Image} from "react-native";
+import {
+  TextInput,
+  TouchableOpacity,
+  Image,
+  KeyboardAvoidingView,
+  Keyboard,
+} from "react-native";
 
+import {TouchableWithoutFeedback} from "react-native-gesture-handler";
 import {useNavigation} from "@react-navigation/native";
 
 // 3rd Party
@@ -58,187 +65,198 @@ const Register = () => {
   });
 
   return (
-    <>
-      <Box my="sm">
-        <Text bold size="xl" center>
-          Register
-        </Text>
-      </Box>
-      <Box center my="sm">
-        <Box bg="white" style={{elevation: 20}} mb="lg" radius={100} w={110}>
-          <Image
-            source={icon}
-            style={{
-              overflow: "hidden",
-              height: 110,
-              width: 110,
-              borderRadius: 100,
-            }}
-          />
-        </Box>
-      </Box>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={async (values, {setSubmitting}) => {
-          setSubmitting(true);
-          const response = await signup({
-            variables: values,
-          });
-          //@ts-ignore
-          setUserToken(response.data.signup.token);
+    <KeyboardAvoidingView style={{flex: 1}} behavior="position">
+      <>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <Box my="sm">
+            <Text bold size="xl" center>
+              Register
+            </Text>
+          </Box>
+          <Box center my="sm">
+            <Box
+              bg="white"
+              style={{elevation: 20}}
+              mb="lg"
+              radius={100}
+              w={110}>
+              <Image
+                source={icon}
+                style={{
+                  overflow: "hidden",
+                  height: 110,
+                  width: 110,
+                  borderRadius: 100,
+                }}
+              />
+            </Box>
+          </Box>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={async (values, {setSubmitting}) => {
+              setSubmitting(true);
+              const response = await signup({
+                variables: values,
+              });
+              //@ts-ignore
+              setUserToken(response.data.signup.token);
 
-          setSubmitting(false);
-        }}>
-        {({handleChange, handleBlur, handleSubmit, values, errors}) => (
-          <>
-            <Box mx={60}>
-              <Box>
-                <Box dir="row" align="center">
-                  <Box position="absolute" right={0} ml="sm">
-                    <AntDesign name="user" size={18} color="black" />
+              setSubmitting(false);
+            }}>
+            {({handleChange, handleBlur, handleSubmit, values, errors}) => (
+              <>
+                <Box mx={60}>
+                  <Box>
+                    <Box dir="row" align="center">
+                      <Box position="absolute" right={0} ml="sm">
+                        <AntDesign name="user" size={18} color="black" />
+                      </Box>
+                      <TextInput
+                        onChangeText={handleChange("name")}
+                        onBlur={handleBlur("name")}
+                        placeholder="Enter your username"
+                        placeholderTextColor="gray"
+                        value={values.name}
+                        style={{
+                          borderBottomWidth: 1,
+                          width: "100%",
+                          borderBottomColor: "gray",
+                        }}
+                      />
+                    </Box>
+                    {errors.name && (
+                      <Text style={{fontSize: 10, color: "red"}}>
+                        {errors.name}
+                      </Text>
+                    )}
                   </Box>
-                  <TextInput
-                    onChangeText={handleChange("name")}
-                    onBlur={handleBlur("name")}
-                    placeholder="Enter your username"
-                    placeholderTextColor="gray"
-                    value={values.name}
-                    style={{
-                      borderBottomWidth: 1,
-                      width: "100%",
-                      borderBottomColor: "gray",
-                    }}
-                  />
-                </Box>
-                {errors.name && (
-                  <Text style={{fontSize: 10, color: "red"}}>
-                    {errors.name}
-                  </Text>
-                )}
-              </Box>
-              <Box>
-                <Box dir="row" align="center">
-                  <Box position="absolute" right={0} ml="sm">
-                    <AntDesign name="mail" size={18} color="black" />
+                  <Box>
+                    <Box dir="row" align="center">
+                      <Box position="absolute" right={0} ml="sm">
+                        <AntDesign name="mail" size={18} color="black" />
+                      </Box>
+                      <TextInput
+                        onChangeText={handleChange("email")}
+                        placeholder="Enter your email address"
+                        onBlur={handleBlur("email")}
+                        keyboardType={"email-address"}
+                        value={values.email}
+                        style={{
+                          borderBottomWidth: 1,
+                          width: "100%",
+                          borderBottomColor: "gray",
+                        }}
+                      />
+                    </Box>
+                    {errors.email && (
+                      <Text style={{fontSize: 10, color: "red"}}>
+                        {errors.email}
+                      </Text>
+                    )}
                   </Box>
-                  <TextInput
-                    onChangeText={handleChange("email")}
-                    placeholder="Enter your email address"
-                    onBlur={handleBlur("email")}
-                    keyboardType={"email-address"}
-                    value={values.email}
-                    style={{
-                      borderBottomWidth: 1,
-                      width: "100%",
-                      borderBottomColor: "gray",
-                    }}
-                  />
-                </Box>
-                {errors.email && (
-                  <Text style={{fontSize: 10, color: "red"}}>
-                    {errors.email}
-                  </Text>
-                )}
-              </Box>
-              <Box>
-                <Box dir="row" align="center">
-                  <Box position="absolute" right={0} ml="sm">
-                    <AntDesign name="lock" size={18} color="black" />
-                  </Box>
+                  <Box>
+                    <Box dir="row" align="center">
+                      <Box position="absolute" right={0} ml="sm">
+                        <AntDesign name="lock" size={18} color="black" />
+                      </Box>
 
-                  <TextInput
-                    onChangeText={handleChange("password")}
-                    onBlur={handleBlur("password")}
-                    placeholder="password"
-                    placeholderTextColor="gray"
-                    secureTextEntry={true}
-                    value={values.password}
-                    style={{
-                      borderBottomWidth: 1,
-                      width: "100%",
-                      borderBottomColor: "gray",
-                    }}
-                  />
-                </Box>
-                {errors.password && (
-                  <Text style={{fontSize: 10, color: "red"}}>
-                    {errors.password}
-                  </Text>
-                )}
-              </Box>
-              <Box>
-                <Box dir="row" align="center">
-                  <Box position="absolute" right={0} ml="sm">
-                    <Feather
-                      color={
-                        values.confirmPassword !== "" && !errors.confirmPassword
-                          ? "green"
-                          : "red"
-                      }
-                      size={18}
-                      name={
-                        values.confirmPassword !== "" && !errors.confirmPassword
-                          ? "check-circle"
-                          : "x-circle"
-                      }
-                    />
+                      <TextInput
+                        onChangeText={handleChange("password")}
+                        onBlur={handleBlur("password")}
+                        placeholder="password"
+                        placeholderTextColor="gray"
+                        secureTextEntry={true}
+                        value={values.password}
+                        style={{
+                          borderBottomWidth: 1,
+                          width: "100%",
+                          borderBottomColor: "gray",
+                        }}
+                      />
+                    </Box>
+                    {errors.password && (
+                      <Text style={{fontSize: 10, color: "red"}}>
+                        {errors.password}
+                      </Text>
+                    )}
                   </Box>
-                  <TextInput
-                    onChangeText={handleChange("confirmPassword")}
-                    onBlur={handleBlur("confirmPassword")}
-                    placeholder="confirm password"
-                    placeholderTextColor="gray"
-                    secureTextEntry={true}
-                    value={values.confirmPassword}
-                    style={{
-                      borderBottomWidth: 1,
-                      width: "100%",
-                      borderBottomColor: "gray",
-                    }}
-                  />
+                  <Box>
+                    <Box dir="row" align="center">
+                      <Box position="absolute" right={0} ml="sm">
+                        <Feather
+                          color={
+                            values.confirmPassword !== "" &&
+                            !errors.confirmPassword
+                              ? "green"
+                              : "red"
+                          }
+                          size={18}
+                          name={
+                            values.confirmPassword !== "" &&
+                            !errors.confirmPassword
+                              ? "check-circle"
+                              : "x-circle"
+                          }
+                        />
+                      </Box>
+                      <TextInput
+                        onChangeText={handleChange("confirmPassword")}
+                        onBlur={handleBlur("confirmPassword")}
+                        placeholder="confirm password"
+                        placeholderTextColor="gray"
+                        secureTextEntry={true}
+                        value={values.confirmPassword}
+                        style={{
+                          borderBottomWidth: 1,
+                          width: "100%",
+                          borderBottomColor: "gray",
+                        }}
+                      />
+                    </Box>
+                    {errors.confirmPassword && (
+                      <Text style={{fontSize: 10, color: "red"}}>
+                        {errors.confirmPassword}
+                      </Text>
+                    )}
+                  </Box>
                 </Box>
-                {errors.confirmPassword && (
-                  <Text style={{fontSize: 10, color: "red"}}>
-                    {errors.confirmPassword}
-                  </Text>
-                )}
-              </Box>
-            </Box>
-            <Box center my="sm">
-              <Box
-                center
-                bg="white"
-                w={100}
-                radius="lg"
-                style={{elevation: 10}}>
-                <TouchableOpacity onPress={handleSubmit}>
-                  <Text p={8} bold>
-                    Register
-                  </Text>
-                </TouchableOpacity>
-              </Box>
-            </Box>
-            {/*Register Links*/}
-            <Box dir="row" justify="center" my={4}>
-              <Text size="sm">already have an account?</Text>
-              <TouchableOpacity
-                style={{marginLeft: 10}}
-                onPress={() => navigation.navigate("Login")}>
-                <Text
-                  size="sm"
-                  bold
-                  style={{
-                    textDecorationLine: "underline",
-                  }}>
-                  login
-                </Text>
-              </TouchableOpacity>
-            </Box>
-          </>
-        )}
-      </Formik>
-    </>
+                <Box center my="sm">
+                  <Box
+                    center
+                    bg="white"
+                    w={100}
+                    radius="lg"
+                    style={{elevation: 10}}>
+                    <TouchableOpacity onPress={handleSubmit}>
+                      <Text p={8} bold>
+                        Register
+                      </Text>
+                    </TouchableOpacity>
+                  </Box>
+                </Box>
+                {/*Register Links*/}
+                <Box dir="row" justify="center" my={4}>
+                  <Text size="sm">already have an account?</Text>
+                  <TouchableOpacity
+                    style={{marginLeft: 10}}
+                    onPress={() => navigation.navigate("Login")}>
+                    <Text
+                      size="sm"
+                      bold
+                      style={{
+                        textDecorationLine: "underline",
+                      }}>
+                      login
+                    </Text>
+                  </TouchableOpacity>
+                </Box>
+              </>
+            )}
+          </Formik>
+        </TouchableWithoutFeedback>
+      </>
+    </KeyboardAvoidingView>
   );
 };
 
